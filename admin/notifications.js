cfdc5e55163361466483a724820eb98d7eaaf7ec
Vinícius Santos <viniciusssantos.pa@gmail.com>
2023-06-24 19:00:10 -0300

    Update notifications.js

--- admin/notifications.js
+++ admin/notifications.js
@@ -1,8 +1,15 @@
 document.querySelector('.notifications-blocks').innerHTML = `
                 <div class="not-block">
+                    <h2>Atualização 0.6 (18:57 de 24/06/2023)</h2>
+                    <p>* Adição de produtos no estoque das pratas</p>
+                    <p>Autor: Vinícius S.</p>
+                    <p style="color: green">Uploaded: Sucess</p>
+                </div>
+                <div class="not-block">
                     <h2>Atualização 0.5 (17:42 de 24/06/2023)</h2>
                     <p>* Finalização da página "Plano Mensal", disponível na página da administração.</p>
                     <p>Autor: Vinícius S.</p>
+                    <p style="color: green">Uploaded: Sucess</p>
                 </div>
                 <div class="not-block">
                     <h2>Atualização 0.4 (21:45 de 23/06/2023)</h2>
@@ -11,6 +18,7 @@ document.querySelector('.notifications-blocks').innerHTML = `
                     <p>* Mudança no estoque das pratas</p>
                     <p>* Início da estrutura básica do site dos pedidos pendentes.</p>
                     <p>Autor: Vinícius S.</p>
+                    <p style="color: green">Uploaded: Sucess</p>
                 </div>
                 <div class="not-block">
                     <h2>Atualização 0.3 (17:58 de 03/06/2023)</h2>
@@ -18,6 +26,7 @@ document.querySelector('.notifications-blocks').innerHTML = `
                     <p>* Modelo para celular concluído</p>
                     <p>* Novas opções no menu que estarão disponíveis em breve</p>
                     <p>Autor: Vinícius</p>
+                    <p style="color: green">Uploaded: Sucess</p>
                 </div>
                 <div class="not-block">
                     <h2>Atualização 0.2 (20:27 de 30/05/2023)</h2>
@@ -28,6 +37,7 @@ document.querySelector('.notifications-blocks').innerHTML = `
                     <p>* -> Widgets Interativos</p>
                     <p>* Opção "Planilha", disponível no website da administração, foi lançada com êxito</p>
                     <p>Autor: Vinícius</p>
+                    <p style="color: green">Uploaded: Sucess</p>
                 </div>
                 <div class="not-block">
                     <h2>Atualização 0.1 (23:00 de 17/05/2023)</h2>
@@ -37,5 +47,6 @@ document.querySelector('.notifications-blocks').innerHTML = `
                     <p>* Mudanças na estrutura da página da administração</p>
                     <p>* Mudanças no estilo da página da administração</p>
                     <p>Autor: Vinícius</p>
+                    <p style="color: green">Uploaded: Sucess</p>
                 </div>
 `
