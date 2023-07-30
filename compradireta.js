6a651a2bec4d1b7615eaa9f789f1a0fb0550c77e
Vinícius Santos <viniciusssantos.pa@gmail.com>
2023-07-30 20:25:07 -0300

    .

--- compradireta.js
+++ compradireta.js
@@ -1319,6 +1319,63 @@ function prata_pulseira_nossa_senhora_comp() {
       })
 }
 
+function prata_colar_gravatinha_comp() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+
+    Swal.fire({
+        title: 'Você deseja adicionar <b>Extensor (+4cm)</b>',
+        icon: 'info',
+        html:
+          `
+          <div class='item-prata'>
+            <img src="../imgs/extensor (1).jpeg" alt="">
+            <div class="text-prata">
+                <p class='novidade-loja'>Novo</p>
+                <h1 class="nome-prata">Extensor (+4cm)</h1>
+                <div class='estrelas'>
+                    <img src="../imgs/Nenhuma estrela.png" alt="">
+                </div>
+                <div class="promocao">
+                    <p class="preço-promo"><s>R$ 13,00</s>
+                    <p class="preço-loja">R$ 8,50</p>
+                    <p class="opcoesdepaga">(comprado junto com produtos)</p>
+                </div>
+                
+            </div>
+        </div>
+    ` +
+          '+ R$ 8,50 na soma total',
+          showCancelButton: true,
+          confirmButtonColor: '#EF59A0',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'ADICIONAR',
+          cancelButtonText: 'NÃO '
+    }).then((result) => {
+        if (result.isConfirmed) {
+            tarefas.push('Colar Gravatinha'  + ' ' + '> Tam: ' + tamanho.value + ' ' + ' | ' + 'Qtd: ' + parseInt(quantidade.value))
+            mostraTarefas()
+            salvarNoLocalStorage()
+            tarefas.push('Extensor'  + ' ' + '> Tam: +4cm' + ' ' + ' | ' + 'Qtd: ' + '1')
+            mostraTarefas()
+            salvarNoLocalStorage()
+            window.location.href = '../resumo.html'
+        } else if (
+            result.dismiss
+        ){
+            tarefas.push('Colar Gravatinha' + ' ' + '> Tam: ' + tamanho.value + ' ' + ' | ' + 'Qtd: ' + parseInt(quantidade.value))
+            mostraTarefas()
+            salvarNoLocalStorage()
+            window.location.href = '../resumo.html'
+        }
+      })
+}
+
 function prata_brinco_coracaotrançado_comp() {
     tarefas.push('Brinco Coração Trançado'  + ' ' + '> Tam: ' + tamanho.value + ' ' + ' | ' + 'Qtd: ' + parseInt(quantidade.value))
     mostraTarefas()
